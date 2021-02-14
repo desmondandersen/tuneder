4b8b535c08e6057e47f4f228630500e9e022e23a
Grace Ma <glm1550@ucla.edu>
2021-02-14 14:38:03 -0800

    finalized from meeting: tabs with routing

--- src/App.js
+++ src/App.js
@@ -5,32 +5,41 @@ import Login from './components/Login.js';
 import Routes from './Routes.js'
 import { LinkContainer } from "react-router-bootstrap"
 
+
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+
 function App() {
 
   
   return (
     <div className = "App">
-      <Navbar collapseOnSelect bg = "light" expand = "md">
-        <LinkContainer to= '/'>
-          <Navbar.Brand>
-            Home
-          </Navbar.Brand>
-        </LinkContainer>
-        <Navbar.Toggle/>
-        <Navbar.Collapse className="justify-content-end">
-          <Nav>
-            <LinkContainer to = '/create_account'>
-              <Nav.Link> Create Account </Nav.Link>
-            </LinkContainer>
-            <LinkContainer to = '/login'>
-              <Nav.Link> Log In </Nav.Link>
-            </LinkContainer>
-            <LinkContainer to = '/my_account'>
-              <Nav.Link> My Account </Nav.Link>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Container>
+        <Navbar collapseOnSelect bg = "light" expand = "md">
+          <LinkContainer to= '/'>
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+
+          <Form inline>
+            <Form.Control type='text' placeholder = "i am looking for a ... " className = "mr-sm-2"/>
+          </Form>
+          <Navbar.Toggle/>
+          <Navbar.Collapse className="justify-content-end">
+            <Nav activeKey={window.location.pathname}>
+              <LinkContainer to = '/create_account'>
+                <Nav.Link> Create Account </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to = '/login'>
+                <Nav.Link> Log In </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to = '/my_account'>
+                <Nav.Link> My Account </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
       <Routes/>
     </div>
   );
