d157d82a40eb61c63f594cda36bd29d57a8fa970
Desmond Andersen <dfa@ucla.edu>
2021-02-20 19:22:17 -0800

    Set up database connection

--- client/src/index.js
+++ client/src/index.js
@@ -7,10 +7,8 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App/>
+      <App />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(cors());
+
+// Connect to mongoDB cluster
+const CONNECTION_URL =
+  'mongodb+srv://dbAdmin:eggert97@cluster0.nayet.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}/`)
+    )
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
