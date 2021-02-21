09f94e8102c6e21627a5d431f6eb8ad2efc84caa
Desmond Andersen <dfa@ucla.edu>
2021-02-20 16:39:53 -0800

    Added logo, refactor to single quotes

--- client/src/components/CreateAccount.js
+++ client/src/components/CreateAccount.js
@@ -1,36 +1,35 @@
-
-import React, { useState } from "react"
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import {Link} from 'react-router-dom';
-import { LinkContainer } from "react-router-bootstrap"
-
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export default function CreateAccount() {
-    
-    function handleClick(){
-        console.log("Button clicked")
-    }
+  function handleClick() {
+    console.log('Button clicked');
+  }
 
-    return(
-        <div className = "Login">
-            <Form >
-                I am a...
-                <br></br>
-                <br></br>
-                <LinkContainer to = '/new_musician'>
-                        <Link> <Button> Musician 
-                        </Button>
-                         </Link>
-                </LinkContainer>
-                <br></br><br></br>
-                <LinkContainer to = '/new_venue'>
-                        <Link> <Button> Venue 
-                        </Button>
-                         </Link>
-                </LinkContainer>
-
-            </Form>
-        </div>
-    )
+  return (
+    <div className='Login'>
+      <Form>
+        I am a...
+        <br></br>
+        <br></br>
+        <LinkContainer to='/new_musician'>
+          <Link>
+            {' '}
+            <Button> Musician</Button>
+          </Link>
+        </LinkContainer>
+        <br></br>
+        <br></br>
+        <LinkContainer to='/new_venue'>
+          <Link>
+            {' '}
+            <Button> Venue</Button>
+          </Link>
+        </LinkContainer>
+      </Form>
+    </div>
+  );
 }
--- client/src/components/VenueProfile.js
+++ client/src/components/VenueProfile.js
@@ -1,19 +1,17 @@
-import React from "react";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 function VenueProfile(props) {
-    return (
-        <Card style={{ width: '30rem'}, {marginTop: '1rem'}}>
-            <Card.Body>
-                <Card.Title>{props.name}</Card.Title>
-                <Card.Text>
-                    {props.artist}
-                </Card.Text>
-                <Button variant="primary">Learn more</Button>
-            </Card.Body>
-        </Card>
-    )
+  return (
+    <Card style={({ width: '30rem' }, { marginTop: '1rem' })}>
+      <Card.Body>
+        <Card.Title>{props.name}</Card.Title>
+        <Card.Text>{props.artist}</Card.Text>
+        <Button variant='primary'>Learn more</Button>
+      </Card.Body>
+    </Card>
+  );
 }
 
-export default VenueProfile
+export default VenueProfile;
