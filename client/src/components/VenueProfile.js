8d06ab77564fdfd6b1c6e8ea28479665bdd99802
Nicholas Dean <NicholasPDean@users.noreply.github.com>
2021-02-28 21:32:00 -0800

    Implement 'Learn more' button for Venue cards.
    Clicking the 'Learn More' button will take you to the
    /venue-info link and display the appropriate venue
    information based off of the Venue card that you click.

--- client/src/components/VenueProfile.js
+++ client/src/components/VenueProfile.js
@@ -1,18 +1,45 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-function VenueProfile(props) {
-  const venues = useSelector((state) => state.venues);
+import { LinkContainer } from 'react-router-bootstrap';
+import Nav from 'react-bootstrap/Nav';
 
+const handleOnSubmit = props => {
+  props.history.push({
+    pathname: '/venue-info',
+    state: {
+      venue_name: this.props.name,
+      email: "test_email", 
+      city: "test_city",
+      phone: "test_phone",
+    }
+  });
+};
+
+function VenueProfile(props) {
   return (
     <Card className='card--venue'>
       <Card.Body>
         <Card.Title>{props.name}</Card.Title>
         <Card.Text>{props.artist}</Card.Text>
-        <Button variant='primary'>Learn more</Button>
+        <Nav activeKey={window.location.pathname}>
+          <LinkContainer to={{
+            pathname: '/venue-info',
+            state: {
+              venue_name: props.name,
+              email: "test_email",
+              city: "test_city",
+              phone: "test_phone",
+            }
+            }}>
+            <Nav.Link>
+              <Button variant='primary'>Learn more</Button>
+            </Nav.Link>
+          </LinkContainer>
+        </Nav>
+        {/* <Button variant='primary'>Learn more</Button> */}
       </Card.Body>
     </Card>
   );
