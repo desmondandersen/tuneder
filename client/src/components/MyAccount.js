a494adbc2d7eba6d3f309ccefbeb8e8eaec58c33
Shalinee Maitra <shalinee30@g.ucla.edu>
2021-03-07 17:04:42 -0800

    worked on nav bar, got rid of bug where new musician page wouldn't redirect, but there's an annoying chrome message

--- client/src/components/MyAccount.js
+++ client/src/components/MyAccount.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import { NavLink } from 'react-router-dom';
+import { NavLink, Redirect } from 'react-router-dom';
 import { withRouter } from 'react-router';
 import ReactPlayer from 'react-player/lazy'
+
 
 // Import bootstrap components
 import { Button } from 'react-bootstrap';
@@ -10,8 +11,132 @@ import { Button } from 'react-bootstrap';
 import VenueInfo from './VenueInfo.js';
 import MusicianInfo from './MusicianInfo.js';
 
+
+// function UserAccountPage(props){
+//   let type = sessionStorage.getItem('type');
+//   if(type == 'Venue')
+//     {
+//         const venue_name = sessionStorage.getItem('name');
+//         const email = sessionStorage.getItem('email');
+//         const address = sessionStorage.getItem('address');
+//         const city = sessionStorage.getItem('city');
+//         const state = sessionStorage.getItem('state');
+//         const zip = sessionStorage.getItem('zip');
+//         const description = sessionStorage.getItem('description');
+
+//         return (
+//             <div className='form-details1'>
+//                 <div className='form-details2'>
+//                     <h2>{venue_name}</h2>
+//                     <p>
+//                         <strong>Description: </strong>
+//                         {description}
+//                         <br />
+//                         <strong>Address: </strong>
+//                         {address}, {city}, {state} {zip}
+//                         <br />
+//                         <strong>Your email: </strong>
+//                         <a href={`mailto:${email}`}>{email} </a>
+//                         <br />
+//                     </p>
+//                     <Button> Edit Info</Button>
+//                 </div>
+//             </div>
+//         );
+//     }
+//     else
+//     {
+//         const musician_name = sessionStorage.getItem('name');
+//         const email = sessionStorage.getItem('email');
+//         const instrument_1 = sessionStorage.getItem('instrument_1');
+//         const instrument_2 = sessionStorage.getItem('instrument_2');
+//         const genre = sessionStorage.getItem('genre');
+//         const bio = sessionStorage.getItem('bio');
+//         let portfolio = sessionStorage.getItem('portfolio');
+      
+//         const renderInstruments = () => {
+//           if (instrument_2)
+//             return (
+//               <ul>
+//                 <li>{instrument_1}</li>
+//                 <li>{instrument_2}</li>
+//               </ul>
+//             );
+//           else return instrument_1;
+//         };
+      
+//         if(portfolio == ""){
+//           portfolio = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+//         }
+      
+//         return (
+//           <div className='form-details1'>
+//             <div className='form-details2'>
+//               <h2>{musician_name}</h2>
+//               <p>
+//                 <strong>Bio: </strong> {bio}
+//                 <br />
+//                 <strong>Genre: </strong> {genre}
+//                 <br />
+//                 <strong>Instruments: </strong> {renderInstruments()}
+//                 <br />
+//                 <strong>Your email: </strong>
+//                 <a href={`mailto:${email}`}>{email} </a>
+//               </p>
+//               <Button> Edit Info</Button>
+//               <NavLink to='/' activeClassName='active'>
+//                 <Button variant='outline-secondary'>See more musicians</Button>
+//               </NavLink>
+//             </div>
+//             <NavLink to='/' activeClassName='active'>
+//               <Button>See more venues</Button>
+//             </NavLink>
+//             <div>
+//               <ReactPlayer
+//                 url={portfolio}
+//                 controls={true}
+//               />
+//             </div>
+//           </div>
+//         );
+//     }
+
+// }
+// function GuestAccountPage(props){
+//   return(
+//     <Redirect
+//         to={{
+//           pathname: '/login',
+//         }}
+//       />
+// )
+// }
+
+// function AccountPage(props){
+//   const isLoggedIn = props.isLoggedIn;
+//   if (isLoggedIn){
+//       return <UserAccountPage />
+//   }
+//   return <GuestAccountPage />
+// }
+
 // Musician Info Page
 const MyAccount = () => {
+  // if (sessionStorage.getItem('isAuthenticated')){
+  //   return (
+  //     <div >
+  //       <AccountPage isLoggedIn={true}/>
+  //     </div>
+  //   );
+  // }
+  // else{
+  //   return(
+  //     <div >
+  //       <AccountPage isLoggedIn={false}/>
+  //   </div>
+  // );
+  // }
+
     let type = sessionStorage.getItem('type');
 
     if(type == 'Venue')
@@ -35,16 +160,11 @@ const MyAccount = () => {
                         <strong>Address: </strong>
                         {address}, {city}, {state} {zip}
                         <br />
-                        <strong>Contact: </strong>
+                        <strong>Your email: </strong>
                         <a href={`mailto:${email}`}>{email} </a>
                         <br />
                     </p>
-                    <Button href={`mailto:${email}`} variant='primary'>
-                        Message
-                    </Button>{' '}
-                    <NavLink to='/' activeClassName='active'>
-                        <Button variant='outline-secondary'>See more venues</Button>
-                    </NavLink>
+                    <Button> Edit Info</Button>
                 </div>
             </div>
         );
@@ -85,12 +205,10 @@ const MyAccount = () => {
                 <br />
                 <strong>Instruments: </strong> {renderInstruments()}
                 <br />
-                <strong>Contact: </strong>
+                <strong>Your email: </strong>
                 <a href={`mailto:${email}`}>{email} </a>
               </p>
-              <Button href={`mailto:${email}`} variant='primary'>
-                Message
-              </Button>{' '}
+              <Button> Edit Info</Button>
               <NavLink to='/' activeClassName='active'>
                 <Button variant='outline-secondary'>See more musicians</Button>
               </NavLink>
@@ -107,6 +225,8 @@ const MyAccount = () => {
           </div>
         );
     }
+
+    
 };
 
 export default withRouter(MyAccount);
