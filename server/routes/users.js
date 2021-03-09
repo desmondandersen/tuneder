7982acfa5efd700e8ea34c7c398a38391bc1886c
Desmond Andersen <dfa@ucla.edu>
2021-03-09 00:35:56 -0800

    Remove extraneous route

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,8 +4,6 @@ import { getUsers, createUser, updateUser } from '../controllers/users.js';
 
 const router = express.Router();
 
-router.get('/login', getUsers);
-
 router.get('/', getUsers);
 router.post('/', createUser);
 router.patch('/:id', updateUser);
